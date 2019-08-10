2e6e399374fb303329509e7463964219497c65d9
Armin Becher <armin.becher@gmail.com>
2019-08-10 15:40:01 +0200

    Fix doctests

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,20 @@
 ///
 /// # Examples
 ///
+/// This call won't panic.
 /// ```rust
 /// # use ntest::assert_true;
-/// #[test]
-/// fn test_assert_true() {
-///    assert_true!(true);
-/// }
+/// # fn main() {
+/// assert_true!(true);
+/// # }
 ///```
 ///
-/// ```rust
-/// #[test]
-/// #[should_panic]
-/// fn test_assert_false_fail() {
-///     assert_false!(true);
-/// }
+/// This call will panic.
+/// ```should_panic
+/// # use ntest::assert_true;
+/// # fn main() {
+/// assert_true!(false);
+/// # }
 /// ```
 #[macro_export]
 macro_rules! assert_true {
@@ -30,21 +30,20 @@ macro_rules! assert_true {
 ///
 /// # Examples
 ///
+/// This call won't panic.
 /// ```rust
 /// # use ntest::assert_false;
-/// #[test]
-/// fn test_assert_false() {
-///     assert_false!(false);
-/// }
+/// # fn main() {
+/// assert_false!(false);
+/// # }
 /// ```
 ///
-/// ```rust
+/// This call will panic.
+/// ```should_panic
 /// # use ntest::assert_false;
-/// #[test]
-/// #[should_panic]
-/// fn test_assert_false_fails() {
-///     assert_false!(true);
-/// }
+/// # fn main() {
+/// assert_false!(true);
+/// # }
 /// ```
 #[macro_export]
 macro_rules! assert_false {
@@ -59,23 +58,22 @@ macro_rules! assert_false {
 /// for more information.
 ///
 /// # Examples
-///
+/// 
+/// This call won't panic.
 /// ```rust
-/// #[test]
-/// fn assert_panic() {
-///    // Here panic can happen!
-///    // ...
-///    assert_panics!({panic!("I am panicing")});
-/// }
+/// # use ntest::assert_panics;
+/// # fn main() {
+/// // Other panics can happen before this call.
+/// assert_panics!({panic!("I am panicing")});
+/// # }
 /// ```
 ///
-/// ```
-///#[test]
-/// #[should_panic]
-///fn test_assert_panics_fail() {
-///    // This call should fail
-///    assert_panics!({println!("I am not panicing")});
-///}
+/// This call will panic.
+/// ```should_panic
+/// # use ntest::assert_panics;
+/// # fn main() {
+/// assert_panics!({println!("I am not panicing")});
+/// # }
 /// ```
 #[macro_export]
 macro_rules! assert_panics {
