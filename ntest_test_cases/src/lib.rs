ebc576c340833ca97596265bdc6899be99c1e412
Armin Becher <armin.becher@gmail.com>
2019-08-11 11:08:34 +0200

    First working proc_macros for test generation

--- ntest_test_cases/src/lib.rs
+++ ntest_test_cases/src/lib.rs
@@ -1,16 +1,58 @@
 extern crate proc_macro;
+extern crate syn;
+use syn::{parse_macro_input, DeriveInput};
 use proc_macro::TokenStream;
+use quote::quote;
 
-#[proc_macro]
-pub fn make_answer(_item: TokenStream) -> TokenStream {
-    "fn answer() -> u32 { 42 }".parse().unwrap()
+#[proc_macro_attribute]
+pub fn test_case(attr: TokenStream, item: TokenStream) -> TokenStream {
+    println!("attr: \"{}\"", attr.to_string());
+    println!("item: \"{}\"", item.to_string());
+    let input = syn::parse_macro_input!(item as syn::ItemFn);
+    let name = &input.ident;
+    let inputs = &input.decl.inputs;
+    //println!("item: \"{}\"", inputs[0].to_string());
+
+    // Our input function is always equivalent to returning 42, right?
+    let result = quote! {
+        #[test]
+        fn #name() {
+            assert!(true);
+        }
+    };
+    result.into()
 }
 
+/*
+#[proc_macro_attribute]
+pub fn test_case(attr: TokenStream, input: TokenStream) -> TokenStream {
+    let attr_string = get_attr_string(&attr);
+    let input_string = format!("#[test_case{}]{}", attr_string, input);
+    let ast          = syn::parse_token_trees(&input_string);
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    match ast {
+        Ok(token_tree) => {
+            let test_case_suit : TestCaseSuit = token_tree.into();
+            let test_cases =
+                test_case_suit
+                    .gen_test_cases()
+                    .to_string();
+
+            TokenStream::from_str(&test_cases)
+                .expect(&format!("generate test cases for: {}", input_string))
+        },
+        Err(e) => panic!(e)
     }
 }
+
+fn get_attr_string(attr: &TokenStream) -> String {
+    let result = format!("{}", attr);
+
+    if result.starts_with("(") {
+        result
+    }
+    else {
+        format!("({})", result)
+    }
+}
+*/
---
+++ ntest_test_cases/tests/test_cases.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate ntest_test_cases;
+#[doc(hidden)]
+pub use ntest_test_cases::test_case;
+
+#[test_case(42)]
+fn one_arg(x: u32) {
+    assert_eq!(x, 42)
+}
+
+
+#[test_case(13)]
+fn one_arg_fail(x: u32) {
+    assert_eq!(x, 42)
+}
+
+
+#[test_case(13, 42)]
+fn two_args(x: u32, y: u32) {
+    assert_eq!(x, 13);
+    assert_eq!(y, 42);
+}
