710f71d64a534f2bea0e8883a1036e0c97ef6252
Armin Becher <armin.becher@gmail.com>
2019-08-12 01:36:32 +0200

    Version update

--- ntest_test_cases/tests/test_cases.rs
+++ ntest_test_cases/tests/test_cases.rs
@@ -2,22 +2,14 @@ extern crate ntest_test_cases;
 #[doc(hidden)]
 pub use ntest_test_cases::test_case;
 
-#[test_case(1)]
-#[test_case(2)]
-#[test_case(3)]
 #[test_case(42)]
 fn one_arg(x: u32) {
     assert_eq!(x, 42)
 }
 
 
-#[test_case(13)]
-fn one_arg_fail(x: u32) {
-    assert_eq!(x, 13)
+#[test_case(13, 42)]
+fn two_args(x: u32, y: u32) {
+    assert_eq!(x, 13);
+    assert_eq!(y, 42);
 }
-
-//#[test_case(m=21,13, 42)]
-//fn two_args(x: u32, y: u32) {
-//    assert_eq!(x, 13);
-//    assert_eq!(y, 42);
-//}
