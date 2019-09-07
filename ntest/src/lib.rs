b2c14249bc03de17c760306c450447ef42319497
Armin Becher <armin.becher@gmail.com>
2019-09-07 13:53:13 +0200

    Add about equal function

--- ntest/src/lib.rs
+++ ntest/src/lib.rs
@@ -5,6 +5,121 @@ extern crate ntest_test_cases;
 #[doc(inline)]
 pub use ntest_test_cases::test_case;
 
+/// Helper trait for `assert_about_equal` macro. Returns the max difference between
+/// two vectors of floats. Can also be used for single floats.  
+///
+/// # Examples
+///
+/// Compare two floating numbers:
+/// ```
+/// # use ntest::MaxDifference;
+/// # fn main() {
+/// assert!((0.1f64 - 42.1f32.max_diff(42.0f32)) < 1.0e-4f64);
+/// # }
+/// ```
+///
+/// Compare two vectors with floating point values. Returns the maximum difference in the vectors:
+/// ```
+/// # use ntest::MaxDifference;
+/// # fn main() {
+/// assert!(0.1f64 - vec![42.0, 42.0f32].max_diff(vec![42.0, 42.1f32]) < 1.0e-4f64);
+/// # }
+/// ```
+pub trait MaxDifference {
+    fn max_diff(self, other: Self) -> f64;
+}
+
+impl MaxDifference for f32 {
+    fn max_diff(self, other: Self) -> f64 {
+        return f64::from((self - other).abs());
+    }
+}
+
+impl MaxDifference for f64 {
+    fn max_diff(self, other: Self) -> f64 {
+        return (self - other).abs();
+    }
+}
+
+impl MaxDifference for Vec<f32> {
+    fn max_diff(self, other: Self) -> f64 {
+        let mut max: f64 = 0.0;
+        for (a, b) in self.iter().zip(other.iter()) {
+            let diff = f64::from((*a - *b).abs());
+            if diff > max {
+                max = diff;
+            }
+        }
+        max
+    }
+}
+
+impl MaxDifference for Vec<f64> {
+    fn max_diff(self, other: Self) -> f64 {
+        let mut max: f64 = 0.0;
+        for (a, b) in self.iter().zip(other.iter()) {
+            let diff = (*a - *b).abs();
+            if diff > max {
+                max = diff;
+            }
+        }
+        max
+    }
+}
+
+/// Compare floating point values or vectors of floating points wether they are approximately equal.
+/// The default value for epsilon is `1.0e-6`.
+///
+/// # Examples
+///
+/// Compare two floating point values which are about equal.
+/// ```
+/// # use ntest::assert_about_eq;
+/// # fn main() {
+/// assert_about_eq!(42.00000001f32, 42.0f32);
+/// # }
+/// ```
+///
+/// Explicitly set an epsilon value. This test should fail.
+/// ```should_fail
+/// # use ntest::assert_about_eq;
+/// # fn main() {
+/// assert_about_eq!(42.00000001f32, 42.0f32, 1.0e-12f32);
+/// # }
+/// ```
+///
+/// Compare two vectors of floats which are about equal.
+/// ```
+/// # use ntest::assert_about_eq;
+/// # fn main() {
+/// assert_about_eq!(vec![1.100000001, 2.1], vec![1.1, 2.1], 0.001f64);
+/// # }
+/// ```
+#[macro_export]
+macro_rules! assert_about_eq {
+    ($a:expr, $b:expr, $eps:expr) => {
+        use $crate::MaxDifference;
+        let eps = $eps;
+        assert!(
+            $a.max_diff($b) < eps,
+            "assertion failed: `(left !== right)` \
+             (left: `{:?}`, right: `{:?}`, epsilon: `{:?}`)",
+            $a,
+            $b,
+            eps
+        );
+    };
+    ($a:expr, $b:expr,$eps:expr,) => {
+        assert_about_eq!($a, $b, $eps);
+    };
+    ($a:expr, $b:expr) => {
+        assert_about_eq!($a, $b, 1.0e-6);
+    };
+    ($a:expr, $b:expr,) => {
+        assert_about_eq!($a, $b, 1.0e-6);
+    };
+}
+
 /// Expects a true expression. Otherwise panics.
 ///
 /// Is an alias for the [assert! macro](https://doc.rust-lang.org/std/macro.assert.html).
@@ -28,14 +143,14 @@ pub use ntest_test_cases::test_case;
 /// ```
 #[macro_export]
 macro_rules! assert_true {
-    ($x:expr) => ({
+    ($x:expr) => {
         if !$x {
             panic!("assertion failed: Expected 'true', but was 'false'");
         }
-    });
-    ($x:expr,) => ({
+    };
+    ($x:expr,) => {
         assert_true!($x);
-    });
+    };
 }
 
 /// Expects a false expression. Otherwise panics.
@@ -59,14 +174,14 @@ macro_rules! assert_true {
 /// ```
 #[macro_export]
 macro_rules! assert_false {
-    ($x:expr) => ({
+    ($x:expr) => {{
         if $x {
             panic!("assertion failed: Expected 'false', but was 'true'");
         }
-    });
-    ($x:expr,) => ({
+    }};
+    ($x:expr,) => {{
         assert_false!($x);
-    });
+    }};
 }
 
 /// A panic in Rust is not always implemented via unwinding, but can be implemented by aborting the
@@ -94,66 +209,104 @@ macro_rules! assert_false {
 /// ```
 #[macro_export]
 macro_rules! assert_panics {
-    ($x:block) => ({
-        let result = std::panic::catch_unwind(||$x);
-        if !result.is_err(){
+    ($x:block) => {{
+        let result = std::panic::catch_unwind(|| $x);
+        if !result.is_err() {
             panic!("assertion failed: code in block did not panic");
         }
-    });
-    ($x:block,) => ({
+    }};
+    ($x:block,) => {{
         assert_panics!($x);
-    });
+    }};
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
     #[test]
     fn assert_true() {
         assert_true!(true);
     }
-    
     #[test]
     #[should_panic]
     fn assert_true_fails() {
         assert_true!(false);
     }
-    
     #[test]
     fn assert_true_trailing_comma() {
         assert_true!(true,);
     }
-    
     #[test]
     fn assert_false() {
         assert_false!(false);
     }
-    
     #[test]
     #[should_panic]
     fn assert_false_fails() {
         assert_false!(true);
     }
-    
     #[test]
     fn assert_false_trailing_comma() {
         assert_false!(false,);
     }
-    
     #[test]
     fn assert_panics() {
-        assert_panics!({panic!("I am panicing!")},);
+        assert_panics!({ panic!("I am panicing!") },);
     }
-    
     #[test]
     #[should_panic]
     fn assert_panics_fails() {
-        assert_panics!({println!("I am not panicing!")},);
+        assert_panics!({ println!("I am not panicing!") },);
     }
-    
     #[test]
     fn assert_panics_trailing_comma() {
-        assert_panics!({panic!("I am panicing!")},);
+        assert_panics!({ panic!("I am panicing!") },);
+    }
+
+    #[test]
+    fn vector() {
+        assert_about_eq!(vec![1.1, 2.1], vec![1.1, 2.1]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn vector_fails() {
+        assert_about_eq!(vec![1.2, 2.1], vec![1.1, 2.1]);
+    }
+
+    #[test]
+    fn vector_trailing_comma() {
+        assert_about_eq!(vec![1.2, 2.1], vec![1.2, 2.1],);
+    }
+
+    #[test]
+    fn vector_trailing_comma_with_epsilon() {
+        assert_about_eq!(vec![1.100000001, 2.1], vec![1.1, 2.1], 0.001f64,);
+    }
+
+    #[test]
+    fn it_should_not_panic_if_values_are_approx_equal() {
+        assert_about_eq!(64f32.sqrt(), 8f32);
+    }
+
+    #[test]
+    fn about_equal_f32() {
+        assert_about_eq!(3f32, 3f32, 1f64);
+    }
+
+    #[test]
+    fn about_equal_f64() {
+        assert_about_eq!(3f64, 3f64);
+    }
+
+    #[test]
+    fn compare_with_epsilon() {
+        assert_about_eq!(42f64, 43f64, 2f64);
+    }
+
+    #[test]
+    #[should_panic]
+    fn fail_with_epsilon() {
+        assert_about_eq!(3f64, 4f64, 1e-8f64);
     }
 }
