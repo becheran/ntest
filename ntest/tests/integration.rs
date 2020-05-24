a02cdd9d8fb76c4e6a41be28491f57c86931ddf7
Armin Becher <armin.becher@methodpark.de>
2020-05-24 20:49:13 +0200

    Fix test

--- ntest/tests/integration.rs
+++ ntest/tests/integration.rs
@@ -11,3 +11,33 @@ fn test_function(i : u32) {
     let sleep_time = time::Duration::from_millis(i);
     thread::sleep(sleep_time);
 }
+
+#[test]
+#[timeout(100)]
+fn no_timeout() {
+    let fifty_millis = time::Duration::from_millis(50);
+    thread::sleep(fifty_millis);
+}
+
+#[test]
+#[timeout(10)]
+#[should_panic]
+fn timeout() {
+    let fifty_millis = time::Duration::from_millis(50);
+    thread::sleep(fifty_millis);
+}
+
+#[test]
+#[timeout(10)]
+#[should_panic]
+fn timeout_inf_loop() {
+    loop {}
+}
+
+#[test]
+#[timeout(100)]
+fn timeout_with_result() -> Result<(), String> {
+    let ten_millis = time::Duration::from_millis(10);
+    thread::sleep(ten_millis);
+    Ok(())
+}
